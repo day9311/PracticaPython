305bca0043193fd31a2c91da0cf13990e1b01f74
day9311 <81270521+day9311@users.noreply.github.com>
2021-03-23 21:17:58 -0300

    Ejercicio de teoría

---
+++ run.py
@@ -0,0 +1,8 @@
+#Desafio 1
+#Ingresar un número desde el teclado e imprimir si el numero es par o no
+
+num=int (input("Ingresa un numero: "))
+if num % 2==0:
+    print ('Es par')
+else:
+    print ('Es impar')
